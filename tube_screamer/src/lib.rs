5ba34a6e6286f109cc60d7c93c4df812c203669c
Dave Mollen <davemollen@gmail.com>
2024-07-20 20:09:51 +0200

    Mix with dry instead of boosted signal

--- tube_screamer/src/lib.rs
+++ tube_screamer/src/lib.rs
@@ -41,7 +41,7 @@ impl TubeScreamer {
   pub fn process(&mut self, input: f32, drive: f32, tone: f32, level: f32) -> f32 {
     let (drive, tone, level) = self.smooth_parameters.process(drive, tone, level);
     let op_amp_output = self.op_amp.process(input, drive);
-    let clip_output = self.clipper.process(op_amp_output) * 0.5 + input;
+    let clip_output = self.clipper.process(op_amp_output) + input;
     let tone_output = self.tone.process(clip_output, tone);
 
     tone_output * level
