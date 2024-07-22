d875c3c84c35e48d6fadd33e2e8b456f7b643279
Dave Mollen <davemollen@gmail.com>
2024-07-22 19:59:20 +0200

    Update clipper.rs

--- tube_screamer/src/clipper.rs
+++ tube_screamer/src/clipper.rs
@@ -22,20 +22,17 @@ impl Clipper {
   pub fn process(&mut self, input: f32) -> f32 {
     let upsampled = self
       .upsample_fir
-      .process(f32x8::splat(input * OVERSAMPLE_FACTOR));
+      .process(f32x8::splat(input * 1.877 * OVERSAMPLE_FACTOR));
     let clipped = Self::clip(upsampled);
-    /*
-      clipping gain is 0.3333 -> 0.66666 * 0.5
-      and the input signal is mixed with the clipped signal
-    */
-    self.downsample_fir.process(clipped).reduce_sum() * 0.33333
+    self.downsample_fir.process(clipped).reduce_sum() * 0.3204805 // 0.640961 * 0.5
   }
 
   fn clip(x: f32x8) -> f32x8 {
-    let x2 = x * x;
-    let x3 = x2 * x;
-    let x5 = x3 * x2;
-    let a = x + f32x8::splat(0.16489087) * x3 + f32x8::splat(0.00985468) * x5;
-    a / (f32x8::splat(1.0) + a * a).sqrt()
+    let x_abs = x.abs();
+    let x2 = x_abs * x_abs;
+    let x4 = x2 * x2;
+    let a = f32x8::splat(1.) + x4;
+
+    x / a.sqrt().sqrt()
   }
 }
