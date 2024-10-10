2fdca8aeaf8a43097f8cb419f75abf9790951781
Dave Mollen <davemollen@gmail.com>
2024-10-10 19:49:55 +0200

    Update dm-tubescreamer.mk

--- .github/workflows/dm-tubescreamer.mk
+++ .github/workflows/dm-tubescreamer.mk
@@ -9,17 +9,14 @@ DM_TUBESCREAMER_SITE = https://github.com/davemollen/dm-TubeScreamer.git
 DM_TUBESCREAMER_SITE_METHOD = git
 DM_TUBESCREAMER_BUNDLES = dm-TubeScreamer.lv2
 
-# Nightly toolchain is needed to enable simd
-define DM_TUBESCREAMER_CONFIGURE_CMDS
-	~/.cargo/bin/rustup toolchain install nightly-2024-08-07
-	~/.cargo/bin/rustup default nightly-2024-08-07
-	~/.cargo/bin/rustup target add $(MOD_PLUGIN_BUILDER_RUST_TARGET)
-endef
-
 define DM_TUBESCREAMER_BUILD_CMDS
+	~/.cargo/bin/rustup default nightly
+
 	rm -f $(@D)/lv2/dm-TubeScreamer.lv2/libdm_tube_screamer.so
 	(cd $(@D)/lv2 && \
 		~/.cargo/bin/cargo build $(MOD_PLUGIN_BUILDER_RUST_BUILD_FLAGS))
+	
+	~/.cargo/bin/rustup default stable
 endef
 
 define DM_TUBESCREAMER_INSTALL_TARGET_CMDS
